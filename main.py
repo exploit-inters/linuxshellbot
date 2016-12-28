5671d598b743a457fa8389c4a70a6ebdf750a4c9
veetaw <vitogerardo01@gmail.com>
2016-12-28 20:08:18 +0100

    now bot can send stderr messages

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import telebot
-import subprocess
+
+from subprocess import Popen, PIPE, STDOUT
+
 from conf import *
 
 bot = telebot.TeleBot(TOKEN)
 
-allowedusers = [243687606, 250552323] #insert your ID
+allowedusers = [243687606] #insert your ID
 
 @bot.message_handler(func=lambda message: True)
 def executecomands(message):
    if message.from_user.id in allowedusers:
       if message.text.startswith('!exec'):
         command = " ".join(message.text.split(" ", 1)[1:])
-        output = subprocess.run(command, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-        bot.send_message(message.chat.id, str(output.stdout))
+        things = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        output = things.stdout.read()
+        bot.send_message(message.chat.id, output)
       return
    return
 bot.polling(none_stop=True)
-
